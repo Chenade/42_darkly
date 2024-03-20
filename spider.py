2675a7af64435a16b2170d38ae5f0c0edd814787
Yangchi <chenade0312@gmail.com>
2024-03-20 18:29:56 +0100

    update spider

--- spider.py
+++ spider.py
@@ -1,28 +1,31 @@
 import scrapy
+import sys
 
 class FolderSpider(scrapy.Spider):
     name = 'folderspider'
     start_urls = ['http://192.168.56.101/.hidden/']
 
     def parse(self, response):
-        # Extracting folder names
         folders = response.css('a::text').extract()
         folders = [folder.strip('/') for folder in folders if folder.endswith('/')]
 
-        # Following links to other folders
         for folder in folders:
             yield response.follow(folder, self.parse_folder)
 
     def parse_folder(self, response):
-        # Extracting README file link
         readme_link = response.css('a[href$="README"]::attr(href)').get()
         if readme_link:
             yield response.follow(readme_link, self.parse_readme)
+        
+        folders = response.css('a::text').extract()
+        folders = [folder.strip('/') for folder in folders if folder.endswith('/')]
+        for folder in folders:
+            yield response.follow(folder, self.parse_folder)
 
     def parse_readme(self, response):
-        # Extracting and yielding README content
-        readme_content = response.body.decode(response.encoding)
+        readme_content = response.body.decode(response.encoding);
+        if 'flag' in readme_content:
+            with open(f'output.txt', 'a') as f:
+                f.write(readme_content)
+            sys.exit()
         folder_name = response.url.split('/')[-2]
-        with open(f'{folder_name}_README.txt', 'w') as f:
-            f.write(readme_content)
-
